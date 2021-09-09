a3335cf3cf4008622046efb963042110c890d324
Juan Gomez <juan.c.gomez574@gmail.com>
2021-09-09 19:04:26 -0400

    Added Reply Code

---
+++ 72HourAssignment.Models/ReplyCreate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _72HourAssignment.Models
+{
+    public class ReplyCreate
+    {
+        [Required]
+        [MinLength(2, ErrorMessage = "Please Enter at least 2 characters.")]
+        [MaxLength(100, ErrorMessage = "There are too many characters in this field.")]
+        public string Text { get; set; }
+        [Required]
+        public int CommentId { get; set; }
+    }
+}
